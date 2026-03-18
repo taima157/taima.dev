827a66aa4418416c4edb62d126b96865f77c546b
Taima <gustavotaima18@gmail.com>
2026-03-18 00:19:12 -0300

    add social section

---
+++ src/components/social-icon/index.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+type SocialIconProps = {
+  url: string;
+  name: string;
+  icon: React.ReactNode;
+};
+
+export function SocialIcon({ url, name, icon }: SocialIconProps) {
+  return (
+    <Link href={url} target="_blank" rel="noopener noreferrer">
+      <div className="border border-white/4 flex gap-2 items-center rounded-md bg-stone-900 p-2">
+        {icon}
+        <span className="text-sm font-medium text-stone-300">{name}</span>
+      </div>
+    </Link>
+  );
+}
---
+++ src/components/social/index.tsx
@@ -0,0 +1,28 @@
+import { Email, GitHub, X } from "@mui/icons-material";
+import { SocialIcon } from "../social-icon";
+
+export function Social() {
+  return (
+    <section className="flex flex-col gap-4">
+      <h2 className="uppercase font-mono text-stone-400 text-md">Social</h2>
+
+      <div className="flex gap-3">
+        <SocialIcon
+          url="https://github.com/taima157"
+          name="github"
+          icon={<GitHub fontSize="small" />}
+        />
+        <SocialIcon
+          url="https://x.com/taima1212kyo"
+          name="twitter"
+          icon={<X fontSize="small" />}
+        />
+        <SocialIcon
+          url="mailto:gustavotaima18@gmail.com"
+          name="e-mail"
+          icon={<Email fontSize="small" />}
+        />
+      </div>
+    </section>
+  );
+}
